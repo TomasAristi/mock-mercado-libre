3a7936d875778e1161bc55b7065f603cccd21de0
TomasAisti <tomasaristimuno@gmail.com>
2022-05-06 16:01:25 -0300

    proyecto al dia 6/05

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")  //requerimos el paquete express
 const path = require ("path")  //path unifica las rutas dentro de los sistemas operatvos((direcciones))
+
 const app = express()  //
 const publicPath = path.resolve(__dirname, "./public") //le indicamos a path que resuelva la siguiente ruta
 app.use(express.static(publicPath))  //usamos la funcion static de express, que recibe como parametro la ruta a la carpeta public
