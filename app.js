ae50586762e7acfd009139b2aca403bd61d419c8
TomasAisti <tomasaristimuno@gmail.com>
2022-05-19 21:29:59 -0300

    creado proctfile

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const { Console } = require("console")
 const express = require("express")  //requerimos el paquete express
 const path = require ("path")  //path unifica las rutas dentro de los sistemas operatvos((direcciones))
 
 const app = express()  //
+
+        const PORT= process.env.PORT || 3080
+
+        app.listen(PORT,()=>{
+            console.log("Corriendo en el puerto "+PORT)
+        })
+
+
 const publicPath = path.resolve(__dirname, "./public") //le indicamos a path que resuelva la siguiente ruta
 app.use(express.static(publicPath))  //usamos la funcion static de express, que recibe como parametro la ruta a la carpeta public
-
-
-app.listen(3080)
 
 app.get("/",(req,res)=>{
     res.sendFile(path.resolve(__dirname, "./views/home.html"))
